dc0b518ed04ce84b5e4bde8d08d186814c6bd559
Omens Studios <administrator@Omenss-MacBook-Pro.local>
2015-06-12 13:33:35 +0800

    Initial Commit

---
+++ UITableViewController Challange Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challange Solution
+//
+//  Created by Omens Studios on 12/6/15.
+//  Copyright (c) 2015 Omens Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewController Challange SolutionTests/UITableViewController_Challange_SolutionTests.m
@@ -0,0 +1,40 @@
+//
+//  UITableViewController_Challange_SolutionTests.m
+//  UITableViewController Challange SolutionTests
+//
+//  Created by Omens Studios on 12/6/15.
+//  Copyright (c) 2015 Omens Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface UITableViewController_Challange_SolutionTests : XCTestCase
+
+@end
+
+@implementation UITableViewController_Challange_SolutionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
